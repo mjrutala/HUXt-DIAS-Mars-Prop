98f98726f7aa6b226093b824066535932db158fb
mjrutala <mrutala@bu.edu>
2023-10-24 16:40:09 +0100

    Removed testing code

--- code/example.py
+++ code/example.py
@@ -18,8 +18,8 @@ import huxt_analysis as HA
 import huxt_inputs as Hin
 
 
-runstart = dt.datetime(2016, 5, 1)
-runend = dt.datetime(2016, 7, 1)
+runstart = dt.datetime(2018, 6, 1)
+runend = dt.datetime(2018, 8, 1)
 simtime = (runend-runstart).days * u.day
 r_min = 329 * u.solRad  #  Average Mars orbital distance
 
@@ -27,11 +27,6 @@ benchmark_starttime = benchmark_time.time()
 
 #   Generate inputs from VSWUM
 time, vcarr, bcarr = H_ad.generate_vCarr_from_VSWUM(runstart, runend)
-
-#!!!! Get spacecraft longitudes during this time span
-#  Or split up the time span dynamically so each HUXt run only needs to be ~2-4ish degrees lon.
-#  Then look into how sampling in 3D works (i.e., with latitude) -- hpopefully theres another easy sampler like sc_series...
-#  Then move this to read_model? Maybe with a renaming as well (Model class? models.py? ...)
 
 #set up the model, with (optional) time-dependent bpol boundary conditions
 model = Hin.set_time_dependent_boundary(vcarr, time, runstart, simtime, 
@@ -44,7 +39,7 @@ model.solve([])
 benchmark_totaltime = benchmark_time.time() - benchmark_starttime
 print('Time elapsed in solving the model: {}'.format(benchmark_totaltime))
 
-HA.plot(model, (0/4.)*simtime)
+#HA.plot(model, (0/4.)*simtime)
 HA.plot(model, (1/4.)*simtime)
 HA.plot(model, (2/4.)*simtime)
 HA.plot(model, (3/4.)*simtime)
